efeb862afb71df342cc58a36a1249a584a53381e
Tandn1902 <tandn1902@gmail.com>
2022-09-30 15:43:11 +0700

    test
    nhat tan test push

---
+++ src/scene/product/CustomerNavigation.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomerNavigation = () => {
+  return (
+    <View>
+      <Text>CustomerNavigation</Text>
+    </View>
+  )
+}
+
+export default CustomerNavigation
